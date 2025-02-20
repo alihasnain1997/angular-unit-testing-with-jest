fc7538f880106ebc32e9c367785fd0b2a0d66ea6
Ali Hasnain <ali.hasnain.9279@gmail.com>
2025-02-20 11:45:09 +0500

    work with mocks

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,17 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { UsersService } from './users.service';
 import { UserInterface } from '../types/user.interface';
+import { UtilsService } from './utils.service';
 
 describe('user service', () => {
   //reference later to our service
   let userService: UsersService;
 
+  //Mock
+  const utilServiceMock = {
+    pluck: jest.fn(),
+  };
   //we must create a testing module to test service inside
   //its a block executed before every single test
   beforeEach(() => {
     //create a testing module, register service inside the module
     TestBed.configureTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: UtilsService, useValue: utilServiceMock },
+      ],
     });
     userService = TestBed.inject(UsersService);
   });
@@ -34,11 +42,20 @@ describe('user service', () => {
 
       expect(userService.users).toEqual([{ id: '3', name: 'ali' }]);
     });
+  });
 
+  describe('remove user', () => {
     it('it should remove the user', () => {
       userService.users = [{ id: '3', name: 'ali' }];
       userService.removeUser('3');
       expect(userService.users).toEqual([]);
+    });
+  });
+
+  describe('get user name', () => {
+    it('it should return username', () => {
+      utilServiceMock.pluck.mockReturnValue(['foo']);
+      expect(userService.getUSernames()).toEqual(['foo']);
     });
   });
 });
